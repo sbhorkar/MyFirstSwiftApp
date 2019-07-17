6d62b7fe0410a7ae35ceacab1269f5b200756785
Rekkno <rekkno.inc@gmail.com>
2019-07-17 13:15:37 -0700

    App Icon and Images

--- MyFirstSwiftApp/ViewController.swift
+++ MyFirstSwiftApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView1: UIImageView!
+    var showFirstImage: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func imageClick(_ sender: Any) {
+        if showFirstImage == false {
+            imageView1.image = UIImage(named: "Bear")
+            showFirstImage = true
+        } else {
+            imageView1.image = UIImage(named: "Eevee")
+            showFirstImage = false
+        }
+    }
+    
 }
 
